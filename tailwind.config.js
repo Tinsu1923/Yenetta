024411ed8f3f8506bfbd55eb83e0f71e638c26fd
Tinsae Daniel <130051678+Tinsu1923@users.noreply.github.com>
2024-10-08 22:15:49 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    colors:{
+        main:'#cbdb2c',
+        dark:'#055f73',
+        back:'#fff4f4',
+        dash:'#50bfa5',
+        over:'#e5f6ff',
+        white:'#ffffff',
+        cloud:'#C5C6D0',
+        coin:'#9897A9',
+        fossil:'#787276',
+        ruby:'#900603',
+        candy:'#D21404',
+        parakeet:'#03C04A',
+        bee:'#FBE106',
+        cyber:'#CC7722',
+        royal:'#101D6B',
+        cobalt:'#0147AB'
+
+
+
+
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
